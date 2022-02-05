2230d9753bdad6fa80cf03eded1abfd3ac181ca7
alperbayata <69384122+alperbayata@users.noreply.github.com>
2022-02-05 12:30:34 +0300

    Update Strings.dart

--- lib/Resource/Strings.dart
+++ lib/Resource/Strings.dart
@@ -1,6 +1,8 @@
 /// Strings hold constant strings used across the package
+import 'package:easy_localization/src/public_ext.dart';
+
 class FlutterPwValidatorStrings {
-  final String atLeast = "At least - character";
+  final String atLeast = "registerScreen.atLeastCharacter".tr();
   final String uppercaseLetters = "- Uppercase letter";
   final String numericCharacters = "- Numeric character";
   final String specialCharacters = "- Special character";
